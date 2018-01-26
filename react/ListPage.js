a14489f57d66890e9fce27e9318bf26138c21fca
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-01-26 14:27:44 -0200

    Create table boiler plate

--- react/ListPage.js
+++ react/ListPage.js
@@ -9,11 +9,83 @@ class Shelf extends Component {
     const { data } = this.props
 
     return (
-      <div>
-        {data.loading
-          ? <FormattedMessage id="store-graphql.loading" />
-          : <pre>{JSON.stringify(data.collections, null, 2)}</pre>
-        }
+      <div className="pa4 w-90 center">
+        <div className="flex justify-between">
+          <div className="fw7 f2">
+            Collections
+          </div>
+          <button>
+            NEW COLLECTION
+          </button>
+        </div>
+        <div className="flex justify-between pt5">
+          <div>
+            Search by collection's name or ID
+          </div>
+          <div>
+            Total of pages: 125
+          </div>
+        </div>
+        <table className="w-100 tl pt5">
+          <thead>
+            <tr>
+              <th className="fw3 gray pb2">NAME</th>
+              <th className="fw3 gray pb2">START</th>
+              <th className="fw3 gray pb2">END</th>
+              <th className="fw3 gray pb2">PRODUCTS</th>
+              <th className="fw3 gray pb2">STATUS</th>
+              <th className="fw3 gray pb2">HIGHLIGHT</th>
+              <th className="fw3 gray pb2">SEARCHABLE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="fw7 pv4 bt b--black-10 w-20">Winter 2018</td>
+              <td className="fw4 pv4 bt b--black-10 w-20">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10 w-20">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10 w-10">65</td>
+              <td className="fw4 pv4 bt b--black-10 w-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10 w-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10 w-10">Inactive</td>
+            </tr>
+            <tr>
+              <td className="fw7 pv4 bt b--black-10">Winter 2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">65</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Inactive</td>
+            </tr>
+            <tr>
+              <td className="fw7 pv4 bt b--black-10">Winter 2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">65</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Inactive</td>
+            </tr>
+            <tr>
+              <td className="fw7 pv4 bt b--black-10">Winter 2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">65</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Inactive</td>
+            </tr>
+            <tr>
+              <td className="fw7 pv4 bt b--black-10">Winter 2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">25/06/2018</td>
+              <td className="fw4 pv4 bt b--black-10">65</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Active</td>
+              <td className="fw4 pv4 bt b--black-10">Inactive</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
