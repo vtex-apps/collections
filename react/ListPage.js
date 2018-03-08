652196842f75b1e988923cbe7e4dbec1dd731196
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-03-08 19:54:13 -0300

    Improve layout

--- react/ListPage.js
+++ react/ListPage.js
@@ -27,103 +27,105 @@ class ListPage extends Component {
     const { data } = this.props
 
     return (
-      <div className="pt8 ph3 w-90 center near-black">
-        <div className="flex justify-between items-center bb b--light-gray pb6">
-          <div className="fw7 f2">
-            Collections
-          </div>
-          <div>
-            <Button primary onClick={this.handleOpenCollection}>
-              New collection
-            </Button>
-          </div>
-        </div>
-        <div className="flex justify-between pt6 w-100">
-          <div className="w-80">
-            <Input htmlProps={{ placeholder: 'Search by collection name…' }} />
-          </div>
-          <div className="flex">
-            <div className="flex bt bl bb b--light-gray bw1 br2 br--left" style={{ visibility: data.variables.page === 1 ? '' : '' }}>
-              <button onClick={this.handlePreviousPage} className="bn f6 pv3 ph4 pointer hover-bg-washed-blue flex items-center">
-                <img src={ArrowLeft} />
-              </button>
+      <div className="pv8 ph3 near-black bg-near-white w-100">
+        <div className="w-90 center">
+          <div className="flex justify-between items-center bb b--light-gray pb6">
+            <div className="fw7 f2">
+              Collections
             </div>
-            <div className="dib">
-              <input className="ba b--light-gray f6 pa3 bw1 dib w2 tc" value={this.props.data.collections.page} />
-            </div>
-            <div className="flex bt br bb b--light-gray bw1 br2 br--right" style={{ visibility: data.collections.totalPages === data.variables.page ? '' : '' }}>
-              <button onClick={this.handleNextPage} className="bn f6 pv3 ph4 pointer hover-bg-washed-blue flex items-center">
-                <img src={ArrowRight} />
-              </button>
+            <div>
+              <Button primary onClick={this.handleOpenCollection}>
+                New collection
+              </Button>
             </div>
           </div>
-        </div>
-        {data.loading
-          ? <FormattedMessage id="loading" />
-          : <div className="w-100">
-              <table className="tl pt8 w-100" cellSpacing="0">
-              <thead className="">
-                <tr>
-                  <th className="fw3 gray pb2 ttu f7 w-40">Name</th>
-                  <th className="fw3 gray pb2 ttu f7 w-20">Start</th>
-                  <th className="fw3 gray pb2 ttu f7 w-20">End</th>
-                  <th className="fw3 gray pb2 ttu f7 w-10">Highlight</th>
-                  <th className="fw3 gray pb2 ttu f7 w-10">Searchable</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.props.data.collections.collections.map(collection => {
-                  return (
-                    <tr
-                      key={collection.id}
-                      className="pointer hover-bg-near-white"
-                      onClick={() => this.handleOpenCollection(collection.id)}
-                    >
-                      <td className="fw7 pv6 bt b--light-gray pl4">
-                        <div className="flex items-center">
-                          <div>
-                            <div className="bg-green dib pa2 br-100"></div>
+          <div className="flex justify-between pt6 w-100">
+            <div className="w-80">
+              <Input htmlProps={{ placeholder: 'Search by collection name…' }} />
+            </div>
+            <div className="flex">
+              <div className="flex bt bb bl b--light-gray bw1 br2 br--left" style={{ visibility: data.variables.page === 1 ? '' : '' }}>
+                <button onClick={this.handlePreviousPage} className="bn f6 pv3 ph4 pointer hover-bg-washed-blue flex items-center">
+                  <img src={ArrowLeft} />
+                </button>
+              </div>
+              <div className="dib">
+                <input className="ba b--light-gray f6 pa3 bw1 dib w2 tc" value={this.props.data.collections.page} />
+              </div>
+              <div className="flex bt br bb b--light-gray bw1 br2 br--right" style={{ visibility: data.collections.totalPages === data.variables.page ? '' : '' }}>
+                <button onClick={this.handleNextPage} className="bn f6 pv3 ph4 pointer hover-bg-washed-blue flex items-center">
+                  <img src={ArrowRight} />
+                </button>
+              </div>
+            </div>
+          </div>
+          {data.loading
+            ? <FormattedMessage id="loading" />
+            : <div className="w-100 bg-white mt7 pv7 br2" style={{ boxShadow: '0 3px 9px 0 rgba(61, 62, 64, 0.2)' }}>
+                <table className="tl pt4 w-90 center" cellSpacing="0">
+                <thead className="">
+                  <tr>
+                    <th className="fw3 gray pb4 ttu f7 w-40-l w-30-ns mt6 pl6">Name</th>
+                    <th className="fw3 gray pb4 ttu f7 w-20 mt6">Start</th>
+                    <th className="fw3 gray pb4 ttu f7 w-20 mt6">End</th>
+                    <th className="fw3 gray pb4 ttu f7 w-10 mt6">Highlight</th>
+                    <th className="fw3 gray pb4 ttu f7 w-10 mt6">Searchable</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.props.data.collections.collections.map(collection => {
+                    return (
+                      <tr
+                        key={collection.id}
+                        className="pointer hover-bg-near-white"
+                        onClick={() => this.handleOpenCollection(collection.id)}
+                      >
+                        <td className="fw7 pv6 bt b--light-gray pl4">
+                          <div className="flex items-center">
+                            <div>
+                              <div className="bg-green dib pa2 br-100"></div>
+                            </div>
+                            <div className="pl3">{collection.name}</div>
                           </div>
-                          <div className="pl3">{collection.name}</div>
-                        </div>
-                      </td>
-                      <td className="fw4 pv6 bt b--light-gray">
-                        <FormattedDate
-                          value={new Date(collection.dateFrom)}
-                          day="2-digit"
-                          month="2-digit"
-                          year="2-digit"
-                          hour="2-digit"
-                          minute="2-digit"
-                        />
-                      </td>
-                      <td className="fw4 pv6 bt b--light-gray">
-                        <FormattedDate
-                          value={new Date(collection.dateTo)}
-                          day="2-digit"
-                          month="2-digit"
-                          year="2-digit"
-                          hour="2-digit"
-                          minute="2-digit"
-                        />
-                      </td>
-                      <td className="fw4 pv6 bt b--light-gray">
-                        <div className={`br-pill ${collection.highlight ? 'bg-washed-blue blue' : 'bg-near-white mid-gray'} f6 pv2 ph3 dib fw5`} >
-                          {collection.highlight ? 'Active' : 'Inactive'}
-                        </div>
-                      </td>
-                      <td className="fw4 pv6 bt b--light-gray">
-                        <div className={`br-pill ${collection.searchable ? 'bg-washed-blue blue' : 'bg-near-white mid-gray'} f6 pv2 ph3 dib fw5`} >
-                          {collection.highlight ? 'Active' : 'Inactive'}
-                        </div>
-                      </td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </table>
-          </div>
-        }
+                        </td>
+                        <td className="fw4 pv6 bt b--light-gray">
+                          <FormattedDate
+                            value={new Date(collection.dateFrom)}
+                            day="2-digit"
+                            month="2-digit"
+                            year="2-digit"
+                            hour="2-digit"
+                            minute="2-digit"
+                          />
+                        </td>
+                        <td className="fw4 pv6 bt b--light-gray">
+                          <FormattedDate
+                            value={new Date(collection.dateTo)}
+                            day="2-digit"
+                            month="2-digit"
+                            year="2-digit"
+                            hour="2-digit"
+                            minute="2-digit"
+                          />
+                        </td>
+                        <td className="fw4 pv6 bt b--light-gray">
+                          <div className={`br-pill ${collection.highlight ? 'bg-washed-blue blue' : 'bg-near-white mid-gray'} f6 pv2 ph3 dib fw5`} >
+                            {collection.highlight ? 'Active' : 'Inactive'}
+                          </div>
+                        </td>
+                        <td className="fw4 pv6 bt b--light-gray">
+                          <div className={`br-pill ${collection.searchable ? 'bg-washed-blue blue' : 'bg-near-white mid-gray'} f6 pv2 ph3 dib fw5`} >
+                            {collection.highlight ? 'Active' : 'Inactive'}
+                          </div>
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
+          }
+        </div>
       </div>
     )
   }
