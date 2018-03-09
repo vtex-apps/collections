ebe6510f841285a7b6ded4b32c08442544cefbe2
ntmedina <nataliamedina.rj@gmail.com>
2018-03-08 21:54:20 -0300

    Add buttons to my collections

--- react/pages/Collection.js
+++ react/pages/Collection.js
@@ -52,27 +52,28 @@ class Collection extends Component {
     const collection = this.state
 
     return (
-      <div className="w-100 bg-near-white h-100">
+      <div className="pv8 ph3 near-black bg-near-white w-100 h-100">
         <div className="w-90 center pv6">
-          <div className="fw7 f2">
-            Collection
-          </div>
-          <DatePicker value={collection.dateFrom} onChange={() => {}} />
-          <TimePicker value={collection.dateFrom} onChange={() => {}} />
-          <DatePicker value={collection.dateTo} onChange={() => {}} />
-          <TimePicker value={collection.dateTo} onChange={() => {}} />
-          <div className="pa6 mt6 br2 shadow-4">
-            <div>
-              <label htmlFor="name">Collection name</label>
-              <div className="pt3">
-                <Input
-                  id="name"
-                  onChange={this.handleChangeName}
-                  placeholder="Name"
-                  value={collection.name}
-                />
-              </div>
+          <div className="flex justify-between items-center bb b--light-gray pb6">
+            <div className="fw7 f2">
+              Collections
             </div>
+            <div>
+              <div className="pr5 dib">
+                <Button onClick={this.handleClickNewCollection}>
+                  cancel
+                </Button>
+              </div>
+              <Button primary onClick={this.handleClickNewCollection}>
+                save
+              </Button>
+            </div>
+          </div>
+          <div className="bg-white pa6 mt6 br2 shadow-4"> 
+            <DatePicker value={collection.dateFrom} onChange={() => {}} />
+            <TimePicker value={collection.dateFrom} onChange={() => {}} />
+            <DatePicker value={collection.dateTo} onChange={() => {}} />
+            <TimePicker value={collection.dateTo} onChange={() => {}} />
             <div className="flex pt5">
               <div className="pr2">
                 <Toggle
@@ -99,6 +100,22 @@ class Collection extends Component {
             </div>
           </div>
 
+          <div className="bg-white pa6 mt6 br2 shadow-4">
+            <div>
+              <label htmlFor="name">Collection name</label>
+              <div className="pt3">
+                <Input
+                  id="name"
+                  onChange={this.handleChangeName}
+                  placeholder="Name"
+                  value={collection.name}
+                />
+              </div>
+            </div>
+
+            
+          </div>
+
           {collection.conditions.map((condition, index) => {
             return (
               <div className="bg-white pa6 mt6 br2 shadow-4" key={index}>
@@ -118,26 +135,6 @@ class Collection extends Component {
               </div>
             )
           })}
-          <div>Hello World!</div>
-          <div className="flex justify-end pt6">
-            <div className="pr3">
-              <Button secondary>
-                Add condition
-              </Button>
-            </div>
-            <div className="mh3" />
-            <div className="pr3">
-              <Button secondary onClick={this.handleCancel}>
-                Cancel
-              </Button>
-            </div>
-            <div className="mh3" />
-            <div>
-              <Button primary>
-                Save
-              </Button>
-            </div>
-          </div>
         </div>
       </div>
     )
