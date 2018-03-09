b6b567e600400ae7fa19cb719e34368a1e2488fb
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-03-09 00:48:47 -0300

    CREATE MAGIC WITH CSS! BEBBER WOULD BE SO PROUD :')

--- react/components/Items/Result.js
+++ react/components/Items/Result.js
@@ -7,35 +7,28 @@ class Result extends Component {
       <div>
         <div className="flex items-center">
           <input type="checkbox" className="mr5"></input>
-          <div className="bt bl bb br b--light-gray pa4 flex items-center w-100">
-            <div className="w-50 flex items-center">
-              <div className="bg-light-gray" style={{ width: '48px', height: '48px' }}></div>
+          <div className="bt bl bb br b--light-gray pa4 flex items-center f6 flex-grow-1 justify-between relative">
+            <div className="flex items-center">
+              <div className="bg-light-gray" style={{ width: '3rem', height: '3rem' }}></div>
               <div className="ml5">Tênis Sk8 Hi-Pro</div>
             </div>
-            <div className="w-50 flex items-center fw3">
-              <div className="w-two-thirds tl">REF: VN0A347TOUD</div>
-              <div className="w-third tl">ID: 2375</div>
+            <div className="flex items-center fw3 justify-end pr7">
+              <div className="tl pr9">REF: VN0A347TOUD</div>
+              <div className="tl">ID: 2375</div>
             </div>
-            <div className="bg-serious-black pa4 br-100 relative" style={{ right: '-1.5rem' }}></div>
+            <div className="bg-serious-black pa4 br-100 absolute" style={{ right: '-0.75rem' }}></div>
           </div>
         </div>
-        <div className="flex items-center justify-end">
+        <div className="flex items-center" style={{ paddingLeft: '3.75rem'}}>
           <input type="checkbox" className="mr5"></input>
-          <div className="bl bb br b--light-gray pa4 flex items-center w-90">
-            <div className="pl6 w-50">Tênis Sk8 Hi-Pro</div>
-            <div className="pl6 w-30">REF: VN0A347TOUD</div>
-            <div className="pl6 w-20">ID: 2375</div>
-            <div className="bg-serious-black pa4 br-100 relative" style={{ right: '-1.5rem' }}></div>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <input type="checkbox" className="mr5"></input>
-          <div className="bt bl bb br b--light-gray pa4 flex items-center w-100">
-            <div className="bg-light-gray" style={{ width: '48px', height: '48px' }}></div>
-            <div className="pl6 w-50">Tênis Sk8 Hi-Pro</div>
-            <div className="pl6 w-30">REF: VN0A347TOUD</div>
-            <div className="pl6 w-20">ID: 2375</div>
-            <div className="bg-serious-black pa4 br-100 relative" style={{ right: '-1.5rem' }}></div>
+          <div className="bl bb br b--light-gray pa4 flex items-center f6 flex-grow-1 justify-between">
+            <div className="flex items-center">
+              <div className="ml2">Tênis Sk8 Hi-Pro</div>
+            </div>
+            <div className="flex items-center fw3 justify-end pr7">
+              <div className="tl pr9">REF: VN0A347TOUD</div>
+              <div className="tl">ID: 2375</div>
+            </div>
           </div>
         </div>
       </div>
