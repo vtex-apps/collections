21733324bce2091f2fd27acb0ba549d1051e0cf7
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-03-09 15:20:54 -0300

    Add emptysearch and emptyicojn

---
+++ react/components/Items/Result/EmptySearch.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import EmptySearchIcon from './EmptySearchIcon'
+
+class EmptySearch extends Component {
+  render() {
+    return (
+      <div
+        className="pa10 tc flex flex-column items-center justify-center mw7 center"
+      >
+        <EmptySearchIcon />
+        <div className="pt5 f4">
+          There's no product or SKU with that name
+        </div>
+        <div className="pt4 f6">
+          Try another name or check if your product is added to the catalog
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EmptySearch
