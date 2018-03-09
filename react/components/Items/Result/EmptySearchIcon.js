a39f5dab18c650c57001f3e3120cee79f6f774a9
Breno Calazans <brenocalazans@gmail.com>
2018-03-09 15:24:37 -0300

    Fix svg

--- react/components/Items/Result/EmptySearchIcon.js
+++ react/components/Items/Result/EmptySearchIcon.js
@@ -8,54 +8,68 @@ class EmptySearchIcon extends Component {
       <svg
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
-        x="0px" y="0px"
+        x="0px"
+        y="0px"
         viewBox="0 0 64 64"
         xmlSpace="preserve"
         width="64"
-        height="64">
-          <g class="nc-icon-wrapper" fill="#368df7">
-            <line
-              data-cap="butt"
-              data-color="color-2"
-              fill="none" stroke={this.props.stroke}
-              stroke-width="2"
-              stroke-miterlimit="10"
-              x1="60" y1="60" x2="43" y2="43"
-              stroke-linejoin="round"
-              stroke-linecap="round">
-            </line>
-            <circle
-              fill="none"
-              stroke={this.props.stroke}
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
-              cx="26" cy="26" r="24"
-              stroke-linejoin="round">
-            </circle>
-            <line
-              data-color="color-2"
-              fill="none"
-              stroke={this.props.stroke}
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
-              transform="rotate(45 26 26)"
-              x1="26" y1="18" x2="26" y2="34"
-              stroke-linejoin="round">
-            </line>
-            <line
-              data-color="color-2"
-              fill="none"
-              stroke={this.props.stroke}
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-miterlimit="10"
-              transform="rotate(45 26 26)"
-              x1="34" y1="26" x2="18" y2="26"
-              stroke-linejoin="round">
-            </line>
-          </g>
+        height="64"
+      >
+        <g fill="#368df7">
+          <line
+            data-cap="butt"
+            data-color="color-2"
+            fill="none"
+            stroke={this.props.stroke}
+            strokeWidth="2"
+            strokeMiterlimit="10"
+            x1="60"
+            y1="60"
+            x2="43"
+            y2="43"
+            strokeLinejoin="round"
+            strokeLinecap="round"
+          />
+          <circle
+            fill="none"
+            stroke={this.props.stroke}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeMiterlimit="10"
+            cx="26"
+            cy="26"
+            r="24"
+            strokeLinejoin="round"
+          />
+          <line
+            data-color="color-2"
+            fill="none"
+            stroke={this.props.stroke}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeMiterlimit="10"
+            transform="rotate(45 26 26)"
+            x1="26"
+            y1="18"
+            x2="26"
+            y2="34"
+            strokeLinejoin="round"
+          />
+          <line
+            data-color="color-2"
+            fill="none"
+            stroke={this.props.stroke}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeMiterlimit="10"
+            transform="rotate(45 26 26)"
+            x1="34"
+            y1="26"
+            x2="18"
+            y2="26"
+            strokeLinejoin="round"
+          />
+        </g>
       </svg>
     )
   }
