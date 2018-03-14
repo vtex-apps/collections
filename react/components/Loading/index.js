1f06b50314c4b27163fa2d1f87c2aa16e861f83e
Breno Calazans <brenocalazans@gmail.com>
2018-03-14 14:05:27 -0300

    Scope Loading CSS

--- react/components/Loading/index.js
+++ react/components/Loading/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import config from 'vtex-tachyons/config.json'
+import style from './style.css'
 
 class Loading extends Component {
   render() {
     return (
       <svg
-        className="spinner"
+        className={style.spinner}
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         x="0px"
@@ -19,13 +20,13 @@ class Loading extends Component {
         <g fill={this.props.fill}>
           <g transform="rotate(43.102857129914426 32 32)">
             <path
-              className="spinner__path"
+              className={style.path}
               opacity="0.4"
               fill={this.props.fill}
               d="M32,64C14.35498,64,0,49.64453,0,32C0,14.35498,14.35498,0,32,0s32,14.35498,32,32 C64,49.64453,49.64502,64,32,64z M32,6C17.66357,6,6,17.66357,6,32c0,14.33691,11.66357,26,26,26s26-11.66309,26-26 C58,17.66357,46.33643,6,32,6z"
             />
             <path
-              className="spinner__path"
+              className={style.path}
               data-color="color-2"
               fill={this.props.fill}
               d="M64,32h-6C58,17.66357,46.33643,6,32,6V0C49.64502,0,64,14.35498,64,32z"
